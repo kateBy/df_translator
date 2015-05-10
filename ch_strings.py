1c027384c03637e1bc2074172a5524ae1f64f6a0
kateBy <nonameisiam@yandex.ru>
2015-05-10 22:29:45 +0300

    Добавлены регулярные выражения для меню Юнитов, навыков, и подписи ничьих животных. Внесены переводы профессий в меню Z

--- ch_strings.py
+++ ch_strings.py
@@ -1,4 +1,9 @@
 
+# -*- coding: utf-8 -*-
+
+import re
+
+
 def load_trans_mo(fn):
     '''Загрузка перевода из файла .mo с помощью библиотеки polib'''
     import polib
@@ -21,23 +26,65 @@ def GetTranslate(_text):
 
     if fromInstance != None:
         return fromInstance.encode("utf-16")+b"\0\0"
-    #else:
-    #    result = ", ".join(Process(text))
-    #    return result.encode("utf-16")+b"\0\0"
+    else:
+        result = Regulars(text)
+        if result != None:
+            return result.encode("utf-16")+b"\0\0"
         
 
    
     return None
 
-def Process(text):
-    words = text.split(", ")
-    for word in words:
-        fromInstance = InstanceStrings.get(word)
-        if fromInstance != None:
-            yield fromInstance
-        else:
-            yield word
-        
+
+WORD_AND_NUM_IN_COMMONS = re.compile(r"[\w/]+?\s\(\d+?\)$") #Citizens (7) | Pets/Livestock (16)
+SKILLS = re.compile(r"(Novice|Adequate|Proficient|Legendary)\s\w+?") #Novice Miner #Adequate Fish Cleaner
+STRAY_ANIMAL = re.compile(r"(Stray)\s\w+?\s\((Tame)\)$") #Stray Hen (Tame)
+#TWO_WORDS = re.compile(r"\w+?\s\w+?") 
+#THREE_WORDS = re.compile(r"\w+\s\w+\s\w+") 
+
+
+
+
+def GET_TRANSLATE(original):
+    translate = InstanceStrings.get(original)
+    if translate == None:
+        return original
+    else:
+        return translate
+
+def PROC_WORD_AND_NUM_IN_COMMONS(text):
+    tmp = text.split(" ") #Делим строку делителем
+    trans = GET_TRANSLATE(tmp[0]) #Запрашиваем перевод первой части
+    return trans + " " + tmp[1]
+
+
+def PROC_SKILLS(text):
+    tmp = text.split(" ")
+    quality = GET_TRANSLATE(tmp[0])
+    skill  = GET_TRANSLATE(" ".join(tmp[1:]))
+    ret = quality + " " + skill
+    return ret
+
+def PROC_STRAY_ANIMAL(text):
+    tmp = text.split(" ")
+    
+
+#Словарь регулярных выражений, состоит из самого выражения и функции-обработчика дла нее
+expressions = {
+WORD_AND_NUM_IN_COMMONS:PROC_WORD_AND_NUM_IN_COMMONS,
+SKILLS:PROC_SKILLS,
+STRAY_ANIMAL:PROC_STRAY_ANIMAL
+}    
+    
+
+def Regulars(text):
+    result = None
+    for exp in expressions:
+        if exp.match(text):                 #Фраза соответствует шаблону
+            result = expressions[exp](text) #Передаём текст на обработчик этого шаблона
+            break
+
+    return result
 
 
 #Сюда вносятся новые строки для теста
