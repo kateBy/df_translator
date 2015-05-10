eac0205f1603804e91cde2dbded5b85f3568798c
kateBy <nonameisiam@yandex.ru>
2015-05-10 13:59:55 +0300

    Начат перевод имён животных. В отладочный скрипт добавлены необхоимые проверки

--- ch_strings.py
+++ ch_strings.py
@@ -21,22 +21,51 @@ def GetTranslate(_text):
 
     if fromInstance != None:
         return fromInstance.encode("utf-16")+b"\0\0"
+    #else:
+    #    result = ", ".join(Process(text))
+    #    return result.encode("utf-16")+b"\0\0"
+        
 
    
     return None
 
+def Process(text):
+    words = text.split(", ")
+    for word in words:
+        fromInstance = InstanceStrings.get(word)
+        if fromInstance != None:
+            yield fromInstance
+        else:
+            yield word
+        
 
 
 #Сюда вносятся новые строки для теста
 InstanceStrings = {}
 
 
-InstanceStrings.update(load_trans_mo("./libs/trans.mo"))
+#Проверка на пустые строки, иначе будет вылет
+checked = True
+for i in InstanceStrings:
+    if InstanceStrings[i] == "":
+        checked = False
+        print("---->",i)
+        
+#Cоздадим исключение, чтобы модуль не загрузился
+assert(checked)
+        
+
+        
+if __name__ != "__main__":
+    InstanceStrings.update(load_trans_mo("./libs/trans.mo"))
 
 def dictToPo(some_dict):
     for item in some_dict:
         print("msgid \"%s\"\nmsgstr \"%s\"\n" % (item, some_dict[item]))
 
 
+if __name__ == "__main__":
+    pass
+    dictToPo(InstanceStrings)
 
     
